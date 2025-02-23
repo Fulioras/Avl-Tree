cb5753d5601966b4c8a3c50919c87c7147d8d241
Augustinas <augustinasbickaitis@gmail.com>
2025-02-23 23:03:49 +0200

    Added main.cpp
    Just a starter file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits.h>
+
+
+struct node {
+    int data;
+    node *left;
+    node *right;
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+
+    return 0;
+}
